fed20d3bb6ffc934a22b5038421e59f1fa46b260
mai ciu s <maicius@outlook.com>
2018-02-25 00:42:21 +0800

    数字界面绘制完成

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,21 +2,43 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+import $ from 'jquery'
 
 class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mnist_array: []
+    };
+  }
+
+  componentDidMount() {
+    $.get('./data/image.json', function (result) {
+      this.setState({
+        mnist_array: result.mnist_array
+      });
+    }.bind(this));
+  }
+
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className="rune">
+        <div className="number">
+          <img src={this.state.mnist_array.image}/>
+          <img src={this.state.mnist_array.image} className="margin-left1"/>
+          <img src={this.state.mnist_array.image} className="margin-left1"/>
+          <img src={this.state.mnist_array.image}/>
+          <img src={this.state.mnist_array.image} className="margin-left1"/>
+          <img src={this.state.mnist_array.image} className="margin-left1"/>
+          <img src={this.state.mnist_array.image}/>
+          <img src={this.state.mnist_array.image} className="margin-left1"/>
+          <img src={this.state.mnist_array.image} className="margin-left1"/>
+        </div>
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
