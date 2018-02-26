19d72857c7f61112e60432e59f50ed7c36fff0d5
mai ciu s <maicius@outlook.com>
2018-02-26 19:12:28 +0800

    update

--- src/components/digit-number.js
+++ src/components/digit-number.js
@@ -25,21 +25,19 @@ class DigitNumber extends React.Component{
   }
   updateNumber(){
     const _seconds = document.querySelectorAll('.seconds');
-
     let digitNumbers = [];
     for(let i =0; i< 5; i++){
       digitNumbers.push(Math.floor(Math.random()*10))
     }
-    console.log(digitNumbers);
     this.setState(() => ({
       digitNumber: digitNumbers
     }));
-    this.setNumber(_seconds[0], this.state.digitNumber[0], 1);
-    this.setNumber(_seconds[1], this.state.digitNumber[1], 1);
-    this.setNumber(_seconds[2], this.state.digitNumber[2], 1);
-    this.setNumber(_seconds[3], this.state.digitNumber[3], 1);
-    this.setNumber(_seconds[4], this.state.digitNumber[4], 1);
-  };
+    this.setNumber(_seconds[0], this.state.digitNumber[0]);
+    this.setNumber(_seconds[1], this.state.digitNumber[1]);
+    this.setNumber(_seconds[2], this.state.digitNumber[2]);
+    this.setNumber(_seconds[3], this.state.digitNumber[3]);
+    this.setNumber(_seconds[4], this.state.digitNumber[4]);
+  }
 
   componentDidMount() {
     this.interval = setInterval(() => this.updateNumber(), 1500);
@@ -49,31 +47,31 @@ class DigitNumber extends React.Component{
     clearInterval(this.interval);
   }
 
-  setNumber(digit, number, on){
+  setNumber(digit, number){
     const that = this;
     let segments = digit.querySelectorAll('.segment');
     let current = parseInt(digit.getAttribute('data-value'));
-    if (!isNaN(current) && current != number) {
+    if (!isNaN(current) && current !== number) {
       // unset previous number
       that.state.digitSegments[current].forEach((digitSegment, index) => {
-        setTimeout(function() {
+        setTimeout(() => {
           segments[digitSegment-1].classList.remove('on');
         }, index)
       });
     }
 
-    if (isNaN(current) || current != number) {
+    if (isNaN(current) || current !== number) {
       // set new number after
-      setTimeout(function() {
+      setTimeout(() => {
         that.state.digitSegments[number].forEach((digitSegment, index) => {
-          setTimeout(function() {
+          setTimeout(() => {
             segments[digitSegment-1].classList.add('on');
           }, index)
         });
       }, 50);
       digit.setAttribute('data-value', number);
     }
-  };
+  }
 
   render(){
     return(
